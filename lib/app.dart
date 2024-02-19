78bbd9e841dc405e690523b98afa8d5df4ba16f6
Anujit-Datta <anujitbestbd@gmail.com>
2024-02-20 02:49:08 +0600

    ...

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_calculator/Presentation/Screens/SplashScreen.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -5,6 +6,18 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BMI Calculator',
+      home: const SplashScreen(),
+      theme: ThemeData.dark().copyWith(
+        primaryColor: const Color(0xFF0A0E21),
+        scaffoldBackgroundColor: const Color(0xFF0A0E21),
+        appBarTheme: const AppBarTheme(
+          centerTitle: true,
+          backgroundColor: Colors.transparent,
+        ),
+      ),
+    );
   }
 }
